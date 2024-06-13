4c906cb05eee11c199049fec2bc7710c57f3e3cf
mir-asif01 <asifbsb2002@gmail.com>
2024-06-13 18:01:29 +0600

    optional rendering added in the navbar

--- src/pages/shared/Navbar.jsx
+++ src/pages/shared/Navbar.jsx
@@ -10,12 +10,13 @@ function Navbar() {
     const [dropDownState, setDropDownState] = useState(false);
     const dropDownMenuRef = useRef();
 
-    const { logOut } = useContext(AuthContext)
+    const { logOut, user, setUser } = useContext(AuthContext)
 
     const handleLogOut = () => {
         logOut()
             .then(res => {
                 toast.success("Logout Successful")
+                setUser(null)
             })
     }
 
@@ -37,33 +38,39 @@ function Navbar() {
     return (
         <nav className="flex items-center justify-between bg-[#393E46] px-4 py-2 text-white">
             <div className="scale-100 cursor-pointer rounded-2xl px-3 py-2 text-2xl font-semibold text-white transition-all duration-200 hover:scale-110">
-                <h2 className='font-semibold'>ProTask</h2>
+                <h2 className='font-semibold'>__ProTask__</h2>
             </div>
             <ul className="hidden items-center justify-between gap-10 md:flex">
                 <li className="text-xl group flex  cursor-pointer flex-col">
                     <NavLink to={"/"}>Home</NavLink>
                     <span className="mt-[2px] h-[3px] w-[0px] rounded-full bg-sky-500 transition-all duration-300 group-hover:w-full"></span>
                 </li>
-                <li className="text-xl group flex  cursor-pointer flex-col">
-                    <NavLink to={"/all-tasks"}>Tasks</NavLink>
-                    <span className="mt-[2px] h-[3px]  w-[0px] rounded-full bg-sky-500 transition-all duration-300 group-hover:w-full"></span>
-                </li>
-                <li className="text-xl group flex  cursor-pointer flex-col">
-                    <NavLink to={"/login"}>Login</NavLink>
-                    <span className="mt-[2px] h-[3px]  w-[0px] rounded-full bg-sky-500 transition-all duration-300 group-hover:w-full"></span>
-                </li>
-                <li className="text-xl group flex  cursor-pointer flex-col">
-                    <NavLink to={"/register"}>Register</NavLink>
-                    <span className="mt-[2px] h-[3px]  w-[0px] rounded-full bg-sky-500 transition-all duration-300 group-hover:w-full"></span>
-                </li>
-                <li className="text-xl group flex  cursor-pointer flex-col">
-                    <button onClick={handleLogOut}>Logout</button>
-                    <span className="mt-[2px] h-[3px]  w-[0px] rounded-full bg-sky-500 transition-all duration-300 group-hover:w-full"></span>
-                </li>
-                <li className="text-xl group flex  cursor-pointer flex-col">
-                    <NavLink to={"/add-review"}>Add Review</NavLink>
-                    <span className="mt-[2px] h-[3px]  w-[0px] rounded-full bg-sky-500 transition-all duration-300 group-hover:w-full"></span>
-                </li>
+                {
+                    user?.email ? <>
+                        <li className="text-xl group flex  cursor-pointer flex-col">
+                            <NavLink to={"/all-tasks"}>Tasks</NavLink>
+                            <span className="mt-[2px] h-[3px]  w-[0px] rounded-full bg-sky-500 transition-all duration-300 group-hover:w-full"></span>
+                        </li>
+                        <li className="text-xl group flex  cursor-pointer flex-col">
+                            <NavLink to={"/add-review"}>Add Review</NavLink>
+                            <span className="mt-[2px] h-[3px]  w-[0px] rounded-full bg-sky-500 transition-all duration-300 group-hover:w-full"></span>
+                        </li>
+                        <li className="text-xl group flex  cursor-pointer flex-col">
+                            <button className="bg-slate-600 px-3 py-2 rounded-lg " onClick={handleLogOut}>Logout</button>
+                            <span className="mt-[2px] h-[3px]  w-[0px] rounded-full bg-sky-500 transition-all duration-300 group-hover:w-full"></span>
+                        </li>
+                    </> :
+                        <>
+                            <li className="text-xl group flex  cursor-pointer flex-col">
+                                <NavLink to={"/login"}>Login</NavLink>
+                                <span className="mt-[2px] h-[3px]  w-[0px] rounded-full bg-sky-500 transition-all duration-300 group-hover:w-full"></span>
+                            </li>
+                            <li className="text-xl group flex  cursor-pointer flex-col">
+                                <NavLink to={"/register"}>Register</NavLink>
+                                <span className="mt-[2px] h-[3px]  w-[0px] rounded-full bg-sky-500 transition-all duration-300 group-hover:w-full"></span>
+                            </li>
+                        </>
+                }
             </ul>
             <div ref={dropDownMenuRef} onClick={() => setDropDownState(!dropDownState)} className="relative flex transition-transform md:hidden">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="cursor-pointer" > <line x1="4" x2="20" y1="12" y2="12" /> <line x1="4" x2="20" y1="6" y2="6" /><line x1="4" x2="20" y1="18" y2="18" /> </svg>
